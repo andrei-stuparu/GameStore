9b4d2da9ba24cc50b88d07a20d21e8d07c55d422
unknown <stuparu>
2018-06-03 10:08:05 +0200

    update 3

--- application/models/customer_model.php
+++ application/models/customer_model.php
@@ -1,5 +1,5 @@
 <?php
-	class Customer_model extends CI_Model{
+	class customer_model extends CI_Model{
 		public function __construct(){
 			$this->load->database();
 		}
--- application/models/game_model.php
+++ application/models/game_model.php
@@ -1,5 +1,5 @@
 <?php
-	class Game_model extends CI_Model{
+	class game_model extends CI_Model{
 		public function __construct(){
 			$this->load->database();
 		}
